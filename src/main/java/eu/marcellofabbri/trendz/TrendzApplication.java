a21c5779bf29ede5b39757a707e2692689b7cc47
Marcello Fabbri <marcellogfabbri@gmail.com>
2020-07-01 20:14:00 +0100

    first commit

---
+++ src/main/java/eu/marcellofabbri/trendz/TrendzApplication.java
@@ -0,0 +1,13 @@
+package eu.marcellofabbri.trendz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/marcellofabbri/trendz/TrendzApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.marcellofabbri.trendz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
