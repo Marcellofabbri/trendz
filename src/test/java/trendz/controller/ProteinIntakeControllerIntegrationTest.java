e8f0aceb0ddb15e4e34372b33fa1f0ea5884a4f6
Marcello Fabbri <marcellogfabbri@gmail.com>
2020-07-14 23:57:34 +0100

    add DB configuration for testing

--- src/test/java/trendz/controller/ProteinIntakeControllerIntegrationTest.java
+++ src/test/java/trendz/controller/ProteinIntakeControllerIntegrationTest.java
@@ -1,19 +1,30 @@
 package trendz.controller;
 
-import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
+import dbConfig.DbConfig;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import trendz.repository.ProteinIntakeRepository;
 
-@RunWith(SpringRunner.class)
-@DataJpaTest
-@AutoConfigureEmbeddedDatabase
-public class ProteinIntakeControllerIntegrationTest {
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {DbConfig.class})
+@ActiveProfiles("DaoTest")
+@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:dao/TestData.sql")
+public class PostgresEmbeddedDaoTestingApplicationTests {
+    @Autowired
+    private ProteinIntakeRepository repo;
     @Test
-    public void test() {
-
+    public void testCustomerSave() {
+        repo.save(Customer.builder()
+                .id(new Random().nextLong())
+                .address("brussels")
+                .name("TestName")
+                .build());
+        Assert.assertTrue(repo.findCustomerByName("TestName") != null);
     }
-
 }
